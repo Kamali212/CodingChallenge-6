f2b3812aa303e4bd23ff4b23cae9f15266c1ca5d
Kamali Hicks <kamalikh3@gmail.com>
2025-02-09 18:35:19 -0500

    Task 4 - Subscription Pricing Model

--- cc_6.js
+++ cc_6.js
@@ -9,11 +9,11 @@ console.log(`Total profit $${profit}`) // Expected output is Total Profit: $3500
 function calculateSalesTax(amount, taxRate) {
     return amount * taxRate; // Create function to calculate sales tax
 }
-const salesTax = calculateSalesTax(100, 0.10); // Calculate sales tax 
+let salesTax = calculateSalesTax(100, 0.10); // Calculate sales tax 
 console.log(`Sales Tax: $${salesTax}`) // Expected output is Sales Tax: $10
 
 // Task 3 Employee Bonus Calculation
-const calculateBonus = (salary, performanceRating) => { // Arrow function to calculate bonus based on performance and salary
+let calculateBonus = (salary, performanceRating) => { // Arrow function to calculate bonus based on performance and salary
     let bonusPercent; // Declaring function variable
     if (performanceRating === "Excellent") { // Using if statements to determine bonus percent based on performance rating 
         bonusPercent = 0.20;
@@ -26,6 +26,20 @@ const calculateBonus = (salary, performanceRating) => { // Arrow function to cal
     return bonus // Return bonus 
 };
 console.log(`Bonus: $${calculateBonus(5000, "Excellent")}`) // Log calculated bonus to console
+
+// Task 4 Parameters and Arguments 
+
+function calculateSubscriptionCost(plan, months, discount = 0) { // Calculate subscription based on plan, months, discount
+let planRate = { "Basic": 10, "Premium": 20, "Enterprise": 50}; // Rates for each plan 
+let planCost = (planRate[plan] * months) - discount; // Create function to determine plan cost using brackets to locate corresponding plan 
+return planCost; // Return plan cost
+}
+console.log(`Total: $${calculateSubscriptionCost("Basic", 6, 10)}`); // Log basic cost with discount to console
+console.log(`Total: $${calculateSubscriptionCost("Premium", 12, 0)}`) // Log premium cost to console 
+
+  
+
+
 
 
 
